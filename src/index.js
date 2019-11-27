35fcf827ede6f1d980ac5dbcbc4d8ac8630d3ca6
Dinh Cong Hieu <hieudc@vnext.com.vn>
2019-11-27 11:31:31 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const AWS = require('aws-sdk')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/status', (req, res) => res.send({ status: "ok" }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
